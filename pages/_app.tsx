9ebc2563cf43c1234031861f0f94f0c377d374c4
Casper-Lee <leecasper2000@gmail.com>
2024-08-02 16:15:43 +0800

    chore: improve fetching performance for forecast and now page Install npm package node-cache

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,7 +2,13 @@ import '@/main.css';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import type { AppProps } from 'next/app';
 
-const queryClient = new QueryClient();
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      staleTime: 30 * 1000,
+    },
+  },
+});
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
--- pages/api/forecast.ts
+++ pages/api/forecast.ts
@@ -1,18 +1,36 @@
 import { NextApiRequest, NextApiResponse } from 'next';
+import NodeCache from 'node-cache';
+
+const cache = new NodeCache({ stdTTL: 30 * 1000 });
 
 export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse,
 ) {
   if (req.method === 'GET') {
+    const cacheKey = 'forecastWeather';
+    const cachedData = cache.get(cacheKey);
+
+    if (cachedData) {
+      return res.status(200).json(cachedData);
+    }
+
     const fetchResponse = await fetch(
       'https://birdsofaweather.netlify.app/api/weather/forecast',
     );
+
     if (fetchResponse.ok) {
       const responseJson = await fetchResponse.json();
+      cache.set(cacheKey, responseJson);
       res.status(200).json(responseJson);
     } else {
-      res.status(500).json({ items: [] });
+      if (cachedData) {
+        res.status(200).json(cachedData);
+      } else {
+        res.status(204).json({
+          error: 'Failed to fetch weather data and no cached data available',
+        });
+      }
     }
   } else {
     res.status(400).json({ error: 'This API only supports GET requests' });
